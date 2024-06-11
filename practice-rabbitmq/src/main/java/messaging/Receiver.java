7a27c1779b252fd0ca43c70a3e60c25cd650e24f
Luan de Oliveira <luan012012@hotmail.com>
2024-06-10 22:18:36 -0300

    feat: criado receiver

---
+++ practice-rabbitmq/src/main/java/messaging/Receiver.java
@@ -0,0 +1,18 @@
+package messaging;
+
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+@Component
+public class Receiver {
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    public void receiveMessage(String message){
+        System.out.println("Receive <" + message + ">");
+        countDownLatch.countDown();
+    }
+    public CountDownLatch getCountDownLatch() {
+        return countDownLatch;
+    }
+}
