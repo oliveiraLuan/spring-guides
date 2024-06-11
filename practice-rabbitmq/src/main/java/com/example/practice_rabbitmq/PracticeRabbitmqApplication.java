e67d95411c2d8e16e7b8487c0e3ecbe14182c6ea
Luan de Oliveira <luan012012@hotmail.com>
2024-06-10 22:07:48 -0300

    Adicionado projeto para pratica com rabbitmq

---
+++ practice-rabbitmq/src/main/java/com/example/practice_rabbitmq/PracticeRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeRabbitmqApplication.class, args);
+	}
+
+}
---
+++ practice-rabbitmq/src/test/java/com/example/practice_rabbitmq/PracticeRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
